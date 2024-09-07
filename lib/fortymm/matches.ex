3608b8507212c86d653212bdbe5d3872237d4ff9
Ryan Morris <mryan.morris@gmail.com>
2024-09-07 00:05:05 +0000

    feat: :sparkles: create a game when a challenge is accepted

--- lib/fortymm/matches.ex
+++ lib/fortymm/matches.ex
@@ -5,6 +5,7 @@ defmodule Fortymm.Matches do
   alias Fortymm.Matches.Match
   alias Fortymm.Matches.MatchParticipant
   alias Fortymm.Accounts.User
+  alias Fortymm.Matches.Game
   alias Ecto.Multi
 
   import Ecto.Query
@@ -23,7 +24,7 @@ defmodule Fortymm.Matches do
   def create_match(_challenge, %User{id: nil}), do: {:error, :invalid_user}
 
   def create_match(%Challenge{id: challenge_id} = challenge, %User{id: accepted_by_id}) do
-    {:ok, %{match: match, challenge: challenge}} =
+    {:ok, %{match: match, challenge: challenge, first_game: first_game}} =
       Multi.new()
       |> Multi.one(
         :existing_challenge,
@@ -33,6 +34,9 @@ defmodule Fortymm.Matches do
         )
       )
       |> Multi.insert(:match, Match.create_from_challenge_changeset(challenge))
+      |> Multi.insert(:first_game, fn %{match: match} ->
+        Game.create_changeset(%Game{}, %{match_id: match.id})
+      end)
       |> Multi.insert_all(
         :match_participants,
         MatchParticipant,
@@ -67,6 +71,6 @@ defmodule Fortymm.Matches do
 
     Updates.broadcast(challenge)
 
-    {:ok, match}
+    {:ok, %{match | games: [first_game]}}
   end
 end
